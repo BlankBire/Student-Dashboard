569df2697310a04fdb92b5e32fa447950080bed2
Đông Sit Khải <23520299@ms.uit.edu.vn>
2025-05-13 10:17:46 +0700

    fix UI for register and login

--- StudentDashboard/GUI/LoginForm.Designer.cs
+++ StudentDashboard/GUI/LoginForm.Designer.cs
@@ -39,39 +39,50 @@
             pbUserName = new PictureBox();
             pbPassword = new PictureBox();
             lbYetAccount = new Label();
+            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pbAvatar).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pbUserName).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pbPassword).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // lbTitle
             // 
             lbTitle.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 163);
             lbTitle.ForeColor = Color.FromArgb(0, 117, 214);
-            lbTitle.Location = new Point(248, 182);
+            lbTitle.Location = new Point(621, 140);
+            lbTitle.Margin = new Padding(2, 0, 2, 0);
             lbTitle.Name = "lbTitle";
-            lbTitle.Size = new Size(489, 62);
+            lbTitle.Size = new Size(344, 47);
             lbTitle.TabIndex = 0;
             lbTitle.Text = "Đăng nhập";
             lbTitle.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // tbUserName
             // 
+            tbUserName.BorderStyle = BorderStyle.None;
             tbUserName.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
-            tbUserName.ForeColor = Color.FromArgb(0, 117, 214);
-            tbUserName.Location = new Point(347, 286);
+            tbUserName.ForeColor = Color.DarkGray;
+            tbUserName.Location = new Point(701, 220);
+            tbUserName.Margin = new Padding(2);
             tbUserName.Name = "tbUserName";
-            tbUserName.Size = new Size(302, 39);
+            tbUserName.Size = new Size(213, 22);
             tbUserName.TabIndex = 2;
+            tbUserName.Text = "Username";
+            tbUserName.TextChanged += tbUserName_TextChanged;
             // 
             // tbPassword
             // 
+            tbPassword.BorderStyle = BorderStyle.None;
             tbPassword.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
-            tbPassword.ForeColor = Color.FromArgb(0, 117, 214);
-            tbPassword.Location = new Point(347, 413);
+            tbPassword.ForeColor = Color.DarkGray;
+            tbPassword.Location = new Point(701, 297);
+            tbPassword.Margin = new Padding(2);
             tbPassword.Name = "tbPassword";
-            tbPassword.Size = new Size(302, 39);
+            tbPassword.Size = new Size(220, 22);
             tbPassword.TabIndex = 4;
+            tbPassword.Text = "Password";
+            tbPassword.TextChanged += tbPassword_TextChanged;
             // 
             // btLogin
             // 
@@ -80,9 +91,10 @@
             btLogin.FlatStyle = FlatStyle.Flat;
             btLogin.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
             btLogin.ForeColor = Color.WhiteSmoke;
-            btLogin.Location = new Point(318, 530);
+            btLogin.Location = new Point(676, 371);
+            btLogin.Margin = new Padding(2);
             btLogin.Name = "btLogin";
-            btLogin.Size = new Size(361, 44);
+            btLogin.Size = new Size(261, 39);
             btLogin.TabIndex = 5;
             btLogin.Text = "Đăng nhập";
             btLogin.UseVisualStyleBackColor = false;
@@ -91,11 +103,12 @@
             // btBack
             // 
             btBack.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
-            btBack.Location = new Point(514, 646);
+            btBack.Location = new Point(818, 440);
+            btBack.Margin = new Padding(2);
             btBack.Name = "btBack";
-            btBack.Size = new Size(147, 42);
+            btBack.Size = new Size(110, 28);
             btBack.TabIndex = 6;
-            btBack.Text = "Quay lại";
+            btBack.Text = "Đăng ký ";
             btBack.UseVisualStyleBackColor = true;
             btBack.Click += btBack_Click;
             // 
@@ -103,9 +116,10 @@
             // 
             pbAvatar.BackColor = Color.Transparent;
             pbAvatar.Image = Properties.Resources.uit_logo1;
-            pbAvatar.Location = new Point(399, 60);
+            pbAvatar.Location = new Point(735, 42);
+            pbAvatar.Margin = new Padding(2);
             pbAvatar.Name = "pbAvatar";
-            pbAvatar.Size = new Size(158, 97);
+            pbAvatar.Size = new Size(139, 79);
             pbAvatar.SizeMode = PictureBoxSizeMode.StretchImage;
             pbAvatar.TabIndex = 15;
             pbAvatar.TabStop = false;
@@ -113,23 +127,26 @@
             // pnUserName
             // 
             pnUserName.BackColor = Color.RoyalBlue;
-            pnUserName.Location = new Point(347, 324);
+            pnUserName.Location = new Point(701, 246);
+            pnUserName.Margin = new Padding(2);
             pnUserName.Name = "pnUserName";
-            pnUserName.Size = new Size(300, 1);
+            pnUserName.Size = new Size(210, 1);
             pnUserName.TabIndex = 17;
             // 
             // pnPassword
             // 
             pnPassword.BackColor = Color.RoyalBlue;
-            pnPassword.Location = new Point(347, 451);
+            pnPassword.Location = new Point(701, 323);
+            pnPassword.Margin = new Padding(2);
             pnPassword.Name = "pnPassword";
-            pnPassword.Size = new Size(300, 1);
+            pnPassword.Size = new Size(210, 1);
             pnPassword.TabIndex = 18;
             // 
             // pbUserName
             // 
             pbUserName.Image = Properties.Resources.person;
-            pbUserName.Location = new Point(307, 286);
+            pbUserName.Location = new Point(666, 222);
+            pbUserName.Margin = new Padding(2);
             pbUserName.Name = "pbUserName";
             pbUserName.Size = new Size(25, 25);
             pbUserName.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -139,7 +156,8 @@
             // pbPassword
             // 
             pbPassword.Image = Properties.Resources.password;
-            pbPassword.Location = new Point(307, 413);
+            pbPassword.Location = new Point(666, 299);
+            pbPassword.Margin = new Padding(2);
             pbPassword.Name = "pbPassword";
             pbPassword.Size = new Size(25, 25);
             pbPassword.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -150,17 +168,31 @@
             // 
             lbYetAccount.AutoSize = true;
             lbYetAccount.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lbYetAccount.Location = new Point(338, 656);
+            lbYetAccount.Location = new Point(695, 446);
+            lbYetAccount.Margin = new Padding(2, 0, 2, 0);
             lbYetAccount.Name = "lbYetAccount";
-            lbYetAccount.Size = new Size(163, 25);
+            lbYetAccount.Size = new Size(108, 15);
             lbYetAccount.TabIndex = 22;
             lbYetAccount.Text = "Chưa có tài khoản?";
             // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.welcome;
+            pictureBox2.Location = new Point(-4, -323);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(605, 1273);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 24;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(998, 813);
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(1012, 659);
+            Controls.Add(pictureBox2);
             Controls.Add(lbYetAccount);
             Controls.Add(pbPassword);
             Controls.Add(pbUserName);
@@ -172,12 +204,14 @@
             Controls.Add(tbPassword);
             Controls.Add(tbUserName);
             Controls.Add(lbTitle);
+            Margin = new Padding(2);
             Name = "LoginForm";
             Text = "Đăng nhập";
             Load += LoginForm_Load;
             ((System.ComponentModel.ISupportInitialize)pbAvatar).EndInit();
             ((System.ComponentModel.ISupportInitialize)pbUserName).EndInit();
             ((System.ComponentModel.ISupportInitialize)pbPassword).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -195,5 +229,6 @@
         private PictureBox pbUserName;
         private PictureBox pbPassword;
         private Label lbYetAccount;
+        private PictureBox pictureBox2;
     }
 }
