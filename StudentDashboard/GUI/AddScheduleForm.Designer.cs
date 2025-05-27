72faf9123977f11b8a09de74b2ebe17f16e158a0
BlankBire <nguyenhuucanhnc2005@gmail.com>
2025-05-27 18:17:04 +0700

    update schedule

--- StudentDashboard/GUI/AddScheduleForm.Designer.cs
+++ StudentDashboard/GUI/AddScheduleForm.Designer.cs
@@ -3,24 +3,20 @@ namespace StudentDashboard.GUI
     partial class AddScheduleForm
     {
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.ComboBox cbDayOfWeek;
+        private System.Windows.Forms.ComboBox cbStartPeriod;
+        private System.Windows.Forms.ComboBox cbEndPeriod;
         private System.Windows.Forms.ComboBox cbSubject;
-        private System.Windows.Forms.Label lblTeacher;
         private System.Windows.Forms.ComboBox cbTeacher;
-        private System.Windows.Forms.Label lblRoom;
         private System.Windows.Forms.ComboBox cbRoom;
-        private System.Windows.Forms.Label lblClass;
         private System.Windows.Forms.ComboBox cbClass;
-        private System.Windows.Forms.Label lblDate;
-        private System.Windows.Forms.DateTimePicker dtpDate;
-        private System.Windows.Forms.Label lblStartTime;
-        private System.Windows.Forms.DateTimePicker dtpStartTime;
-        private System.Windows.Forms.Label lblEndTime;
-        private System.Windows.Forms.DateTimePicker dtpEndTime;
-        private System.Windows.Forms.Label lblSemester;
-        private System.Windows.Forms.TextBox txtSemester;
-        private System.Windows.Forms.Label lblAcademicYear;
-        private System.Windows.Forms.NumericUpDown numAcademicYear;
+        private System.Windows.Forms.Label lblDayOfWeek;
+        private System.Windows.Forms.Label lblStartPeriod;
+        private System.Windows.Forms.Label lblEndPeriod;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.Label lblTeacher;
+        private System.Windows.Forms.Label lblRoom;
+        private System.Windows.Forms.Label lblClass;
         private System.Windows.Forms.Button btnSave;
 
         protected override void Dispose(bool disposing)
@@ -34,245 +30,160 @@ namespace StudentDashboard.GUI
 
         private void InitializeComponent()
         {
-            lblSubject = new Label();
-            cbSubject = new ComboBox();
-            lblTeacher = new Label();
-            cbTeacher = new ComboBox();
-            lblRoom = new Label();
-            cbRoom = new ComboBox();
-            lblClass = new Label();
-            cbClass = new ComboBox();
-            lblDate = new Label();
-            dtpDate = new DateTimePicker();
-            lblStartTime = new Label();
-            dtpStartTime = new DateTimePicker();
-            lblEndTime = new Label();
-            dtpEndTime = new DateTimePicker();
-            lblSemester = new Label();
-            txtSemester = new TextBox();
-            lblAcademicYear = new Label();
-            numAcademicYear = new NumericUpDown();
-            btnSave = new Button();
-            ((System.ComponentModel.ISupportInitialize)numAcademicYear).BeginInit();
-            SuspendLayout();
+            components = new System.ComponentModel.Container();
+
+            lblSubject = new System.Windows.Forms.Label();
+            cbSubject = new System.Windows.Forms.ComboBox();
+            lblTeacher = new System.Windows.Forms.Label();
+            cbTeacher = new System.Windows.Forms.ComboBox();
+            lblRoom = new System.Windows.Forms.Label();
+            cbRoom = new System.Windows.Forms.ComboBox();
+            lblClass = new System.Windows.Forms.Label();
+            cbClass = new System.Windows.Forms.ComboBox();
+            lblDayOfWeek = new System.Windows.Forms.Label();
+            cbDayOfWeek = new System.Windows.Forms.ComboBox();
+            lblStartPeriod = new System.Windows.Forms.Label();
+            cbStartPeriod = new System.Windows.Forms.ComboBox();
+            lblEndPeriod = new System.Windows.Forms.Label();
+            cbEndPeriod = new System.Windows.Forms.ComboBox();
+            btnSave = new System.Windows.Forms.Button();
+
             // 
             // lblSubject
             // 
-            lblSubject.AutoSize = true;
-            lblSubject.Location = new Point(21, 36);
-            lblSubject.Margin = new Padding(2, 0, 2, 0);
-            lblSubject.Name = "lblSubject";
-            lblSubject.Size = new Size(55, 15);
-            lblSubject.TabIndex = 0;
             lblSubject.Text = "Môn học";
+            lblSubject.Location = new System.Drawing.Point(30, 30);
+            lblSubject.Size = new System.Drawing.Size(70, 23);
             // 
             // cbSubject
             // 
             cbSubject.DropDownStyle = ComboBoxStyle.DropDownList;
-            cbSubject.Location = new Point(105, 34);
-            cbSubject.Margin = new Padding(2, 2, 2, 2);
-            cbSubject.Name = "cbSubject";
-            cbSubject.Size = new Size(176, 23);
-            cbSubject.TabIndex = 1;
+            cbSubject.Location = new System.Drawing.Point(120, 30);
+            cbSubject.Size = new System.Drawing.Size(200, 23);
+
             // 
             // lblTeacher
             // 
-            lblTeacher.AutoSize = true;
-            lblTeacher.Location = new Point(21, 60);
-            lblTeacher.Margin = new Padding(2, 0, 2, 0);
-            lblTeacher.Name = "lblTeacher";
-            lblTeacher.Size = new Size(56, 15);
-            lblTeacher.TabIndex = 2;
             lblTeacher.Text = "Giáo viên";
+            lblTeacher.Location = new System.Drawing.Point(30, 70);
+            lblTeacher.Size = new System.Drawing.Size(70, 23);
             // 
             // cbTeacher
             // 
             cbTeacher.DropDownStyle = ComboBoxStyle.DropDownList;
-            cbTeacher.Location = new Point(105, 58);
-            cbTeacher.Margin = new Padding(2, 2, 2, 2);
-            cbTeacher.Name = "cbTeacher";
-            cbTeacher.Size = new Size(176, 23);
-            cbTeacher.TabIndex = 3;
+            cbTeacher.Location = new System.Drawing.Point(120, 70);
+            cbTeacher.Size = new System.Drawing.Size(200, 23);
+
             // 
             // lblRoom
             // 
-            lblRoom.AutoSize = true;
-            lblRoom.Location = new Point(21, 84);
-            lblRoom.Margin = new Padding(2, 0, 2, 0);
-            lblRoom.Name = "lblRoom";
-            lblRoom.Size = new Size(42, 15);
-            lblRoom.TabIndex = 4;
             lblRoom.Text = "Phòng";
+            lblRoom.Location = new System.Drawing.Point(30, 110);
+            lblRoom.Size = new System.Drawing.Size(70, 23);
             // 
             // cbRoom
             // 
             cbRoom.DropDownStyle = ComboBoxStyle.DropDownList;
-            cbRoom.Location = new Point(105, 82);
-            cbRoom.Margin = new Padding(2, 2, 2, 2);
-            cbRoom.Name = "cbRoom";
-            cbRoom.Size = new Size(176, 23);
-            cbRoom.TabIndex = 5;
+            cbRoom.Location = new System.Drawing.Point(120, 110);
+            cbRoom.Size = new System.Drawing.Size(200, 23);
+
             // 
             // lblClass
             // 
-            lblClass.AutoSize = true;
-            lblClass.Location = new Point(21, 108);
-            lblClass.Margin = new Padding(2, 0, 2, 0);
-            lblClass.Name = "lblClass";
-            lblClass.Size = new Size(27, 15);
-            lblClass.TabIndex = 6;
             lblClass.Text = "Lớp";
+            lblClass.Location = new System.Drawing.Point(30, 150);
+            lblClass.Size = new System.Drawing.Size(70, 23);
             // 
             // cbClass
             // 
             cbClass.DropDownStyle = ComboBoxStyle.DropDownList;
-            cbClass.Location = new Point(105, 106);
-            cbClass.Margin = new Padding(2, 2, 2, 2);
-            cbClass.Name = "cbClass";
-            cbClass.Size = new Size(176, 23);
-            cbClass.TabIndex = 7;
+            cbClass.Location = new System.Drawing.Point(120, 150);
+            cbClass.Size = new System.Drawing.Size(200, 23);
+
             // 
-            // lblDate
+            // lblDayOfWeek
             // 
-            lblDate.AutoSize = true;
-            lblDate.Location = new Point(21, 132);
-            lblDate.Margin = new Padding(2, 0, 2, 0);
-            lblDate.Name = "lblDate";
-            lblDate.Size = new Size(35, 15);
-            lblDate.TabIndex = 8;
-            lblDate.Text = "Ngày";
+            lblDayOfWeek.Text = "Thứ";
+            lblDayOfWeek.Location = new System.Drawing.Point(30, 190);
+            lblDayOfWeek.Size = new System.Drawing.Size(70, 23);
             // 
-            // dtpDate
+            // cbDayOfWeek
             // 
-            dtpDate.Location = new Point(105, 130);
-            dtpDate.Margin = new Padding(2, 2, 2, 2);
-            dtpDate.Name = "dtpDate";
-            dtpDate.Size = new Size(176, 23);
-            dtpDate.TabIndex = 9;
+            cbDayOfWeek.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbDayOfWeek.Items.AddRange(new object[] {
+                "Thứ 2", "Thứ 3", "Thứ 4", "Thứ 5", "Thứ 6", "Thứ 7", "Chủ nhật"
+            });
+            cbDayOfWeek.Location = new System.Drawing.Point(120, 190);
+            cbDayOfWeek.Size = new System.Drawing.Size(200, 23);
+
             // 
-            // lblStartTime
+            // lblStartPeriod
             // 
-            lblStartTime.AutoSize = true;
-            lblStartTime.Location = new Point(21, 156);
-            lblStartTime.Margin = new Padding(2, 0, 2, 0);
-            lblStartTime.Name = "lblStartTime";
-            lblStartTime.Size = new Size(68, 15);
-            lblStartTime.TabIndex = 10;
-            lblStartTime.Text = "Giờ bắt đầu";
+            lblStartPeriod.Text = "Tiết bắt đầu";
+            lblStartPeriod.Location = new System.Drawing.Point(30, 230);
+            lblStartPeriod.Size = new System.Drawing.Size(70, 23);
             // 
-            // dtpStartTime
+            // cbStartPeriod
             // 
-            dtpStartTime.CustomFormat = "HH:mm";
-            dtpStartTime.Format = DateTimePickerFormat.Custom;
-            dtpStartTime.Location = new Point(105, 154);
-            dtpStartTime.Margin = new Padding(2, 2, 2, 2);
-            dtpStartTime.Name = "dtpStartTime";
-            dtpStartTime.ShowUpDown = true;
-            dtpStartTime.Size = new Size(176, 23);
-            dtpStartTime.TabIndex = 11;
-            dtpStartTime.ValueChanged += dtpStartTime_ValueChanged;
+            cbStartPeriod.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbStartPeriod.Items.AddRange(new object[] {
+                "Tiết 1", "Tiết 2", "Tiết 3", "Tiết 4", "Tiết 5", "Tiết 6", "Tiết 7", "Tiết 8", "Tiết 9", "Tiết 10"
+            });
+            cbStartPeriod.Location = new System.Drawing.Point(120, 230);
+            cbStartPeriod.Size = new System.Drawing.Size(200, 23);
+
             // 
-            // lblEndTime
+            // lblEndPeriod
             // 
-            lblEndTime.AutoSize = true;
-            lblEndTime.Location = new Point(21, 180);
-            lblEndTime.Margin = new Padding(2, 0, 2, 0);
-            lblEndTime.Name = "lblEndTime";
-            lblEndTime.Size = new Size(71, 15);
-            lblEndTime.TabIndex = 12;
-            lblEndTime.Text = "Giờ kết thúc";
+            lblEndPeriod.Text = "Tiết kết thúc";
+            lblEndPeriod.Location = new System.Drawing.Point(30, 270);
+            lblEndPeriod.Size = new System.Drawing.Size(70, 23);
             // 
-            // dtpEndTime
+            // cbEndPeriod
             // 
-            dtpEndTime.CustomFormat = "HH:mm";
-            dtpEndTime.Format = DateTimePickerFormat.Custom;
-            dtpEndTime.Location = new Point(105, 178);
-            dtpEndTime.Margin = new Padding(2, 2, 2, 2);
-            dtpEndTime.Name = "dtpEndTime";
-            dtpEndTime.ShowUpDown = true;
-            dtpEndTime.Size = new Size(176, 23);
-            dtpEndTime.TabIndex = 13;
-            // 
-            // lblSemester
-            // 
-            lblSemester.AutoSize = true;
-            lblSemester.Location = new Point(21, 204);
-            lblSemester.Margin = new Padding(2, 0, 2, 0);
-            lblSemester.Name = "lblSemester";
-            lblSemester.Size = new Size(44, 15);
-            lblSemester.TabIndex = 14;
-            lblSemester.Text = "Học kỳ";
-            // 
-            // txtSemester
-            // 
-            txtSemester.Location = new Point(105, 202);
-            txtSemester.Margin = new Padding(2, 2, 2, 2);
-            txtSemester.Name = "txtSemester";
-            txtSemester.Size = new Size(176, 23);
-            txtSemester.TabIndex = 15;
-            // 
-            // lblAcademicYear
-            // 
-            lblAcademicYear.AutoSize = true;
-            lblAcademicYear.Location = new Point(21, 228);
-            lblAcademicYear.Margin = new Padding(2, 0, 2, 0);
-            lblAcademicYear.Name = "lblAcademicYear";
-            lblAcademicYear.Size = new Size(56, 15);
-            lblAcademicYear.TabIndex = 16;
-            lblAcademicYear.Text = "Năm học";
-            // 
-            // numAcademicYear
-            // 
-            numAcademicYear.Location = new Point(105, 226);
-            numAcademicYear.Margin = new Padding(2, 2, 2, 2);
-            numAcademicYear.Maximum = new decimal(new int[] { 2100, 0, 0, 0 });
-            numAcademicYear.Minimum = new decimal(new int[] { 2000, 0, 0, 0 });
-            numAcademicYear.Name = "numAcademicYear";
-            numAcademicYear.Size = new Size(175, 23);
-            numAcademicYear.TabIndex = 17;
-            numAcademicYear.Value = new decimal(new int[] { 2024, 0, 0, 0 });
+            cbEndPeriod.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbEndPeriod.Items.AddRange(new object[] {
+                "Tiết 1", "Tiết 2", "Tiết 3", "Tiết 4", "Tiết 5", "Tiết 6", "Tiết 7", "Tiết 8", "Tiết 9", "Tiết 10"
+            });
+            cbEndPeriod.Location = new System.Drawing.Point(120, 270);
+            cbEndPeriod.Size = new System.Drawing.Size(200, 23);
+
             // 
             // btnSave
             // 
-            btnSave.Location = new Point(105, 252);
-            btnSave.Margin = new Padding(2, 2, 2, 2);
-            btnSave.Name = "btnSave";
-            btnSave.Size = new Size(84, 24);
-            btnSave.TabIndex = 18;
             btnSave.Text = "Lưu";
-            btnSave.UseVisualStyleBackColor = true;
-            btnSave.Click += btnSave_Click;
+            btnSave.Location = new System.Drawing.Point(120, 320);
+            btnSave.Size = new System.Drawing.Size(100, 35);
+            btnSave.Click += new System.EventHandler(this.btnSave_Click);
+
             // 
-            // AddScheduleForm
+            // Add controls to the form
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(315, 292);
-            Controls.Add(lblSubject);
-            Controls.Add(cbSubject);
-            Controls.Add(lblTeacher);
-            Controls.Add(cbTeacher);
-            Controls.Add(lblRoom);
-            Controls.Add(cbRoom);
-            Controls.Add(lblClass);
-            Controls.Add(cbClass);
-            Controls.Add(lblDate);
-            Controls.Add(dtpDate);
-            Controls.Add(lblStartTime);
-            Controls.Add(dtpStartTime);
-            Controls.Add(lblEndTime);
-            Controls.Add(dtpEndTime);
-            Controls.Add(lblSemester);
-            Controls.Add(txtSemester);
-            Controls.Add(lblAcademicYear);
-            Controls.Add(numAcademicYear);
-            Controls.Add(btnSave);
-            Margin = new Padding(2, 2, 2, 2);
-            Name = "AddScheduleForm";
-            Text = "Thêm thời khóa biểu";
-            ((System.ComponentModel.ISupportInitialize)numAcademicYear).EndInit();
-            ResumeLayout(false);
-            PerformLayout();
+            this.Controls.Add(lblSubject);
+            this.Controls.Add(cbSubject);
+            this.Controls.Add(lblTeacher);
+            this.Controls.Add(cbTeacher);
+            this.Controls.Add(lblRoom);
+            this.Controls.Add(cbRoom);
+            this.Controls.Add(lblClass);
+            this.Controls.Add(cbClass);
+            this.Controls.Add(lblDayOfWeek);
+            this.Controls.Add(cbDayOfWeek);
+            this.Controls.Add(lblStartPeriod);
+            this.Controls.Add(cbStartPeriod);
+            this.Controls.Add(lblEndPeriod);
+            this.Controls.Add(cbEndPeriod);
+            this.Controls.Add(btnSave);
+
+            // 
+            // Form settings
+            // 
+            this.Text = "Đăng ký môn học";
+            this.ClientSize = new System.Drawing.Size(370, 400);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
         }
     }
-} 
+}
--- StudentDashboard/GUI/AddScheduleForm.cs
+++ StudentDashboard/GUI/AddScheduleForm.cs
@@ -46,31 +46,64 @@ namespace StudentDashboard.GUI
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            // Validate dữ liệu (có thể mở rộng thêm)
-            if (cbSubject.SelectedItem == null || cbTeacher.SelectedItem == null || cbRoom.SelectedItem == null || cbClass.SelectedItem == null)
+            // Validate dữ liệu
+            if (cbSubject.SelectedItem == null || cbTeacher.SelectedItem == null ||
+                cbRoom.SelectedItem == null || cbClass.SelectedItem == null ||
+                cbDayOfWeek.SelectedIndex == -1 || cbStartPeriod.SelectedIndex == -1 || cbEndPeriod.SelectedIndex == -1)
             {
                 MessageBox.Show("Vui lòng chọn đầy đủ thông tin.");
                 return;
             }
-            if (dtpStartTime.Value.TimeOfDay >= dtpEndTime.Value.TimeOfDay)
+
+            int startPeriod = cbStartPeriod.SelectedIndex;
+            int endPeriod = cbEndPeriod.SelectedIndex;
+            if (startPeriod >= endPeriod)
             {
-                MessageBox.Show("Giờ kết thúc phải sau giờ bắt đầu.");
+                MessageBox.Show("Tiết bắt đầu phải nhỏ hơn tiết kết thúc.");
                 return;
             }
-            // Lấy giờ bắt đầu và kết thúc, ép giây về 0
-            var start = dtpStartTime.Value;
-            var end = dtpEndTime.Value;
-            var startTime = new TimeSpan(start.Hour, start.Minute, 0);
-            var endTime = new TimeSpan(end.Hour, end.Minute, 0);
-            var date = dtpDate.Value.Date;
+
+            // Chuyển thứ về int (Thứ 2 = 1, Chủ nhật = 7)
+            int dayOfWeek = cbDayOfWeek.SelectedIndex + 1;
+
+            // Bảng ánh xạ tiết -> thời gian bắt đầu
+            TimeSpan[] periodStartTimes = new[]
+            {
+                new TimeSpan(7,30,0),   // Tiết 1
+                new TimeSpan(8,15,0),   // Tiết 2
+                new TimeSpan(9,0,0),    // Tiết 3
+                new TimeSpan(10,0,0),   // Tiết 4
+                new TimeSpan(10,45,0),  // Tiết 5
+                new TimeSpan(13,0,0),   // Tiết 6
+                new TimeSpan(13,45,0),  // Tiết 7
+                new TimeSpan(14,30,0),  // Tiết 8
+                new TimeSpan(15,30,0),  // Tiết 9
+                new TimeSpan(16,15,0)   // Tiết 10
+            };
+                        // Bảng ánh xạ tiết -> thời gian kết thúc
+                        TimeSpan[] periodEndTimes = new[]
+                        {
+                new TimeSpan(8,15,0),   // Kết thúc tiết 1
+                new TimeSpan(9,0,0),    // Kết thúc tiết 2
+                new TimeSpan(9,45,0),   // Kết thúc tiết 3
+                new TimeSpan(10,45,0),  // Kết thúc tiết 4
+                new TimeSpan(11,30,0),  // Kết thúc tiết 5
+                new TimeSpan(13,45,0),  // Kết thúc tiết 6
+                new TimeSpan(14,30,0),  // Kết thúc tiết 7
+                new TimeSpan(15,15,0),  // Kết thúc tiết 8
+                new TimeSpan(16,15,0),  // Kết thúc tiết 9
+                new TimeSpan(17,0,0)    // Kết thúc tiết 10
+            };
+
+            TimeSpan startTime = periodStartTimes[startPeriod];
+            TimeSpan endTime = periodEndTimes[endPeriod];
 
             var scheduleDAL = new ScheduleDAL();
-            var userSchedules = scheduleDAL.GetSchedulesByUserAndDate(_userId, date);
+            var userSchedules = scheduleDAL.GetSchedulesByUserAndDay(_userId, dayOfWeek); 
 
             bool IsOverlapping(TimeSpan s1, TimeSpan e1, TimeSpan s2, TimeSpan e2) =>
                 s1 < e2 && s2 < e1;
 
-            // Kiểm tra trùng lịch người dùng
             foreach (var s in userSchedules)
             {
                 if (s.start_time.HasValue && s.end_time.HasValue &&
@@ -81,7 +114,7 @@ namespace StudentDashboard.GUI
                 }
             }
 
-            // Tạo đối tượng Schedule (có UserID)
+            // Tạo đối tượng Schedule
             NewSchedule = new Schedule
             {
                 user_id = _userId,
@@ -89,11 +122,14 @@ namespace StudentDashboard.GUI
                 teacher_name = ((Teacher)cbTeacher.SelectedItem).teacher_name,
                 room = ((Room)cbRoom.SelectedItem).room_name,
                 class_name = ((ClassModel)cbClass.SelectedItem).class_name,
-                date = date,
+                day_of_week = dayOfWeek,
                 start_time = startTime,
                 end_time = endTime,
-                semester = txtSemester.Text.Trim(),
-                academic_year = (int)numAcademicYear.Value
+                title = ((Subject)cbSubject.SelectedItem).subject_name, 
+                description = "",
+                date = DateTime.Today, 
+                time = startTime,     
+                create_at = DateTime.Now
             };
             this.DialogResult = DialogResult.OK;
             this.Close();
