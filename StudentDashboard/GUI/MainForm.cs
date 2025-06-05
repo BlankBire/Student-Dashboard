95f667740d5129f313fea0e44f1d3164b1e7d84b
BlankBire <nguyenhuucanhnc2005@gmail.com>
2025-06-05 23:06:59 +0700

    fix title bar main page

--- StudentDashboard/GUI/MainForm.cs
+++ StudentDashboard/GUI/MainForm.cs
@@ -8,11 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Guna.UI2.WinForms;
+using System.Runtime.InteropServices;
 
 namespace StudentDashboard.GUI
 {
     public partial class MainForm : Form
     {
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
         public MainForm()
         {
             InitializeComponent();
@@ -30,6 +36,40 @@ namespace StudentDashboard.GUI
         private void MainForm_Load(object sender, EventArgs e)
         {
         }
+        // Xử lý kéo form
+        private void pnTitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        // Đóng form
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Thu nhỏ form
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        // Phóng to/thu nhỏ form
+        private void btnMaximize_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+                btnMaximize.Text = "❐"; // Icon restore
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+                btnMaximize.Text = "□"; // Icon maximize
+            }
+        }
+
 
         private void btRegister_Click(object sender, EventArgs e)
         {
