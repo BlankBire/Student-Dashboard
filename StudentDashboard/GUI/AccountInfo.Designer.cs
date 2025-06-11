90bfa5d1196b33ca7d6bdadc864689a3c737b474
BlankBire <nguyenhuucanhnc2005@gmail.com>
2025-06-12 02:19:16 +0700

    update UI for account info page

--- StudentDashboard/GUI/AccountInfo.Designer.cs
+++ StudentDashboard/GUI/AccountInfo.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace StudentDashboard.GUI
+﻿using Guna.UI2.WinForms;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StudentDashboard.GUI
 {
     partial class AccountInfo
     {
@@ -28,128 +32,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            tbUserName = new TextBox();
-            tbEmail = new TextBox();
-            tbCreateAT = new TextBox();
-            btLogout = new Button();
-            btChangePassword = new Button();
-            btChangeInfo = new Button();
-            btHome = new Button();
+            lblTitle = new Label();
+            lblUserName = new Label();
+            lblEmail = new Label();
+            lblCreateAt = new Label();
+            tbUserName = new Guna2TextBox();
+            tbEmail = new Guna2TextBox();
+            tbCreateAT = new Guna2TextBox();
+            btLogout = new Guna2Button();
+            btChangePassword = new Guna2Button();
+            btChangeInfo = new Guna2Button();
+            btHome = new Guna2Button();
+            iconUser = new PictureBox();
+            iconMail = new PictureBox();
+            iconCalendar = new PictureBox();
             SuspendLayout();
-            // 
-            // tbUserName
-            // 
-            tbUserName.Font = new Font("Segoe UI", 18F);
-            tbUserName.ForeColor = Color.Aqua;
-            tbUserName.Location = new Point(412, 178);
-            tbUserName.Name = "tbUserName";
-            tbUserName.Size = new Size(634, 55);
-            tbUserName.TabIndex = 1;
-            // 
-            // tbEmail
-            // 
-            tbEmail.Font = new Font("Segoe UI", 18F);
-            tbEmail.ForeColor = Color.Cyan;
-            tbEmail.Location = new Point(412, 278);
-            tbEmail.Name = "tbEmail";
-            tbEmail.Size = new Size(634, 55);
-            tbEmail.TabIndex = 3;
-            // 
-            // tbCreateAT
-            // 
-            tbCreateAT.Font = new Font("Segoe UI", 18F);
-            tbCreateAT.ForeColor = Color.Cyan;
-            tbCreateAT.Location = new Point(412, 362);
-            tbCreateAT.Name = "tbCreateAT";
-            tbCreateAT.Size = new Size(634, 55);
-            tbCreateAT.TabIndex = 5;
-            // 
-            // btLogout
-            // 
-            btLogout.BackColor = SystemColors.ButtonFace;
-            btLogout.Font = new Font("Segoe UI", 15F);
-            btLogout.ForeColor = Color.FromArgb(0, 188, 212);
-            btLogout.Location = new Point(871, 524);
-            btLogout.Name = "btLogout";
-            btLogout.Size = new Size(191, 77);
-            btLogout.TabIndex = 6;
-            btLogout.Text = "Đăng xuất";
-            btLogout.UseVisualStyleBackColor = false;
-            btLogout.Click += btLogout_Click;
-            // 
-            // btChangePassword
-            // 
-            btChangePassword.BackColor = Color.FromArgb(182, 204, 254);
-            btChangePassword.FlatAppearance.BorderSize = 0;
-            btChangePassword.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point, 163);
-            btChangePassword.ForeColor = Color.FromArgb(13, 71, 161);
-            btChangePassword.Location = new Point(534, 524);
-            btChangePassword.Name = "btChangePassword";
-            btChangePassword.Size = new Size(234, 77);
-            btChangePassword.TabIndex = 7;
-            btChangePassword.Text = "Đổi mật khẩu";
-            btChangePassword.UseVisualStyleBackColor = false;
-            btChangePassword.Click += btChangePassword_Click;
-            // 
-            // btChangeInfo
-            // 
-            btChangeInfo.BackColor = Color.FromArgb(160, 132, 220);
-            btChangeInfo.FlatAppearance.BorderSize = 0;
-            btChangeInfo.FlatStyle = FlatStyle.Popup;
-            btChangeInfo.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point, 163);
-            btChangeInfo.ForeColor = Color.White;
-            btChangeInfo.Location = new Point(121, 524);
-            btChangeInfo.Name = "btChangeInfo";
-            btChangeInfo.Size = new Size(336, 77);
-            btChangeInfo.TabIndex = 8;
-            btChangeInfo.Text = "Đổi thông tin cá nhân";
-            btChangeInfo.UseVisualStyleBackColor = false;
-            btChangeInfo.Click += btChangeInfo_Click;
-            // 
-            // btHome
-            // 
-            btHome.AutoSize = true;
-            btHome.FlatAppearance.BorderSize = 0;
-            btHome.FlatStyle = FlatStyle.Flat;
-            btHome.Font = new Font("Segoe UI", 12F);
-            btHome.ForeColor = SystemColors.Desktop;
-            btHome.Image = Properties.Resources.icons8_home_24;
-            btHome.Location = new Point(1093, 14);
-            btHome.Margin = new Padding(4, 5, 4, 5);
-            btHome.Name = "btHome";
-            btHome.Size = new Size(118, 70);
-            btHome.TabIndex = 9;
-            btHome.UseVisualStyleBackColor = true;
+
+            int formWidth = 1224;
+            int formHeight = 659;
+            int groupWidth = 500;
+            int leftX = (formWidth - groupWidth) / 2;
+            int blockHeight = 70; // icon+label+textbox
+            int blockSpacing = 35;
+            int numBlocks = 3;
+            int totalInfoHeight = numBlocks * blockHeight + (numBlocks - 1) * blockSpacing;
+            int topY = (formHeight - totalInfoHeight - 120) / 2; // 120 là title + nút
+
+            // btHome (icon home, góc phải, căn giữa icon)
+            btHome.Text = "";
+            btHome.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btHome.FillColor = Color.FromArgb(60, 130, 246);
+            btHome.BorderRadius = 18;
+            btHome.ForeColor = Color.White;
+            btHome.Size = new Size(48, 48);
+            btHome.Location = new Point(formWidth - 70, 24);
+            btHome.Image = Image.FromFile("resources/icons8-home-24.png");
+            btHome.ImageAlign = HorizontalAlignment.Center;
+            btHome.ImageSize = new Size(28, 28);
+            btHome.HoverState.FillColor = Color.FromArgb(80, 150, 255);
+            btHome.HoverState.ForeColor = Color.White;
             btHome.Click += btHome_Click;
-            // 
+
+            // lblTitle
+            lblTitle.Text = "Thông tin tài khoản";
+            lblTitle.Font = new Font("Segoe UI", 22, FontStyle.Bold);
+            lblTitle.ForeColor = Color.FromArgb(94, 148, 255);
+            lblTitle.AutoSize = true;
+            lblTitle.Location = new Point(leftX + (groupWidth - lblTitle.PreferredWidth) / 2, topY);
+
+            // Block 1: UserName
+            int block1Y = topY + 60;
+            iconUser.Image = Image.FromFile("resources/person.png");
+            iconUser.SizeMode = PictureBoxSizeMode.Zoom;
+            iconUser.Size = new Size(24, 24);
+            iconUser.Location = new Point(leftX, block1Y);
+            lblUserName.Text = "Tên đăng nhập";
+            lblUserName.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            lblUserName.ForeColor = Color.FromArgb(60, 130, 246);
+            lblUserName.AutoSize = true;
+            lblUserName.Location = new Point(leftX + 32, block1Y + 2);
+            tbUserName.Font = new Font("Segoe UI", 11F);
+            tbUserName.PlaceholderText = "";
+            tbUserName.ReadOnly = true;
+            tbUserName.BorderRadius = 10;
+            tbUserName.BorderColor = Color.FromArgb(200, 220, 255);
+            tbUserName.FillColor = Color.FromArgb(245, 247, 250);
+            tbUserName.Location = new Point(leftX, block1Y + 28);
+            tbUserName.Size = new Size(groupWidth, 38);
+
+            // Block 2: Email
+            int block2Y = block1Y + blockHeight + blockSpacing;
+            iconMail.Image = Image.FromFile("resources/mail.png");
+            iconMail.SizeMode = PictureBoxSizeMode.Zoom;
+            iconMail.Size = new Size(24, 24);
+            iconMail.Location = new Point(leftX, block2Y);
+            lblEmail.Text = "Email";
+            lblEmail.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            lblEmail.ForeColor = Color.FromArgb(60, 130, 246);
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(leftX + 32, block2Y + 2);
+            tbEmail.Font = new Font("Segoe UI", 11F);
+            tbEmail.PlaceholderText = "";
+            tbEmail.ReadOnly = true;
+            tbEmail.BorderRadius = 10;
+            tbEmail.BorderColor = Color.FromArgb(200, 220, 255);
+            tbEmail.FillColor = Color.FromArgb(245, 247, 250);
+            tbEmail.Location = new Point(leftX, block2Y + 28);
+            tbEmail.Size = new Size(groupWidth, 38);
+
+            // Block 3: CreateAt
+            int block3Y = block2Y + blockHeight + blockSpacing;
+            iconCalendar.Image = Image.FromFile("resources/calendar.png");
+            iconCalendar.SizeMode = PictureBoxSizeMode.Zoom;
+            iconCalendar.Size = new Size(24, 24);
+            iconCalendar.Location = new Point(leftX, block3Y);
+            lblCreateAt.Text = "Ngày tạo tài khoản";
+            lblCreateAt.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            lblCreateAt.ForeColor = Color.FromArgb(60, 130, 246);
+            lblCreateAt.AutoSize = true;
+            lblCreateAt.Location = new Point(leftX + 32, block3Y + 2);
+            tbCreateAT.Font = new Font("Segoe UI", 11F);
+            tbCreateAT.PlaceholderText = "";
+            tbCreateAT.ReadOnly = true;
+            tbCreateAT.BorderRadius = 10;
+            tbCreateAT.BorderColor = Color.FromArgb(200, 220, 255);
+            tbCreateAT.FillColor = Color.FromArgb(245, 247, 250);
+            tbCreateAT.Location = new Point(leftX, block3Y + 28);
+            tbCreateAT.Size = new Size(groupWidth, 38);
+
+            // Các nút căn giữa, ngay dưới cụm thông tin
+            int buttonWidth = 160;
+            int buttonHeight = 38;
+            int buttonSpacing = 40;
+            int totalButtonWidth = buttonWidth * 3 + buttonSpacing * 2;
+            int buttonStartX = leftX + (groupWidth - totalButtonWidth) / 2;
+            int buttonY = block3Y + blockHeight + 40;
+
+            btChangeInfo.Text = "Đổi thông tin cá nhân";
+            btChangeInfo.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            btChangeInfo.FillColor = Color.FromArgb(94, 148, 255);
+            btChangeInfo.BorderRadius = 8;
+            btChangeInfo.ForeColor = Color.White;
+            btChangeInfo.Size = new Size(buttonWidth, buttonHeight);
+            btChangeInfo.Location = new Point(buttonStartX, buttonY);
+            btChangeInfo.Click += btChangeInfo_Click;
+
+            btChangePassword.Text = "Đổi mật khẩu";
+            btChangePassword.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            btChangePassword.FillColor = Color.FromArgb(94, 148, 255);
+            btChangePassword.BorderRadius = 8;
+            btChangePassword.ForeColor = Color.White;
+            btChangePassword.Size = new Size(buttonWidth, buttonHeight);
+            btChangePassword.Location = new Point(buttonStartX + buttonWidth + buttonSpacing, buttonY);
+            btChangePassword.Click += btChangePassword_Click;
+
+            btLogout.Text = "Đăng xuất";
+            btLogout.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            btLogout.FillColor = Color.FromArgb(220, 38, 38);
+            btLogout.BorderRadius = 8;
+            btLogout.ForeColor = Color.White;
+            btLogout.Size = new Size(buttonWidth, buttonHeight);
+            btLogout.Location = new Point(buttonStartX + (buttonWidth + buttonSpacing) * 2, buttonY);
+            btLogout.Click += btLogout_Click;
+
             // AccountInfo
-            // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackgroundImage = Properties.Resources.Your_paragraph_text;
+            BackColor = Color.FromArgb(230, 240, 255);
             ClientSize = new Size(1224, 659);
             Controls.Add(btHome);
+            Controls.Add(lblTitle);
+            Controls.Add(iconUser);
+            Controls.Add(lblUserName);
+            Controls.Add(tbUserName);
+            Controls.Add(iconMail);
+            Controls.Add(lblEmail);
+            Controls.Add(tbEmail);
+            Controls.Add(iconCalendar);
+            Controls.Add(lblCreateAt);
+            Controls.Add(tbCreateAT);
             Controls.Add(btChangeInfo);
             Controls.Add(btChangePassword);
             Controls.Add(btLogout);
-            Controls.Add(tbCreateAT);
-            Controls.Add(tbEmail);
-            Controls.Add(tbUserName);
-            ForeColor = Color.Azure;
             Name = "AccountInfo";
             Text = "Thông tin tài khoản";
             Load += AccountInfo_Load;
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-        private TextBox tbUserName;
-        private TextBox tbEmail;
-        private TextBox tbCreateAT;
-        private Button btLogout;
-        private Button btChangePassword;
-        private Button btChangeInfo;
-        private Button btHome;
+        private Label lblTitle;
+        private Label lblUserName;
+        private Label lblEmail;
+        private Label lblCreateAt;
+        private Guna2TextBox tbUserName;
+        private Guna2TextBox tbEmail;
+        private Guna2TextBox tbCreateAT;
+        private Guna2Button btLogout;
+        private Guna2Button btChangePassword;
+        private Guna2Button btChangeInfo;
+        private Guna2Button btHome;
+        private PictureBox iconUser;
+        private PictureBox iconMail;
+        private PictureBox iconCalendar;
     }
 }
